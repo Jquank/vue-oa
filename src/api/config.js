44860fd927916e1629fdc9979f8183ad0ce206b8
Jquank <478197650@qq.com>
2018-06-09 20:15:50 +0800

    0619 update addBaiduOrder.vue

--- src/api/config.js
+++ src/api/config.js
@@ -1,44 +1,45 @@
 export const serverUrl = 'http://172.16.12.87:8080/BaiJieOA'
-export const navList = [{
-  id: 1,
-  fontIcon: 'fa fa-cogs',
-  text: '系统配置',
-  cList: [{
-    cid: 30,
-    to: '/indexPage/rule',
-    text: '权限设置'
+export const navList = [
+  {
+    id: 1,
+    fontIcon: 'fa fa-cogs',
+    text: '系统配置',
+    cList: [{
+      cid: 30,
+      to: '/indexPage/rule',
+      text: '权限设置'
+    },
+    {
+      cid: 31,
+      to: '/indexPage/auth',
+      text: '角色设置'
+    },
+    {
+      cid: 32,
+      to: '/indexPage/systemCycle',
+      text: '时长设置'
+    },
+    {
+      cid: 33,
+      to: '/indexPage/departSetNum',
+      text: '部门配额'
+    },
+    {
+      cid: 34,
+      to: '/indexPage/accountManage',
+      text: '账号管理'
+    },
+    {
+      cid: 35,
+      to: '/indexPage/news',
+      text: '公告管理'
+    }
+    ]
   },
   {
-    cid: 31,
-    to: '/indexPage/auth',
-    text: '角色设置'
-  },
-  {
-    cid: 32,
-    to: '/indexPage/systemCycle',
-    text: '时长设置'
-  },
-  {
-    cid: 33,
-    to: '/indexPage/departSetNum',
-    text: '部门配额'
-  },
-  {
-    cid: 34,
-    to: '/indexPage/accountManage',
-    text: '账号管理'
-  },
-  {
-    cid: 35,
-    to: '/indexPage/news',
-    text: '公告管理'
-  }
-  ]
-},
-{
-  id: 2,
-  fontIcon: 'fa fa-street-view',
-  text: '人员管理'
+    id: 2,
+    fontIcon: 'fa fa-street-view',
+    text: '人员管理'
   // cList: [
   //   { cid: 40, to: "/indexPage/rule", text: "权限设置" },
   //   { cid: 41, to: "/indexPage/auth", text: "角色设置" },
@@ -47,47 +48,47 @@ export const navList = [{
   //   { cid: 44, to: "/indexPage/accountManage", text: "账号管理" },
   //   { cid: 45, to: "/indexPage/news", text: "公告管理" },
   // ],
-},
-{
-  id: 3,
-  fontIcon: 'fa fa-user',
-  text: '客户管理',
-  cList: [{
-    cid: 50,
-    to: '/indexPage',
-    text: '新增客户'
   },
   {
-    cid: 51,
-    to: '/indexPage',
-    text: '申领客户'
+    id: 3,
+    fontIcon: 'fa fa-user',
+    text: '客户管理',
+    cList: [{
+      cid: 50,
+      to: '/indexPage',
+      text: '新增客户'
+    },
+    {
+      cid: 51,
+      to: '/indexPage',
+      text: '申领客户'
+    },
+    {
+      cid: 52,
+      to: '/indexPage/myCustomer',
+      text: '我的客户'
+    },
+    {
+      cid: 53,
+      to: '/indexPage',
+      text: '出访记录'
+    },
+    {
+      cid: 54,
+      to: '/indexPage',
+      text: '跟进记录'
+    },
+    {
+      cid: 55,
+      to: '/indexPage',
+      text: '导入客户'
+    }
+    ]
   },
   {
-    cid: 52,
-    to: '/indexPage/myCustomer',
-    text: '我的客户'
-  },
-  {
-    cid: 53,
-    to: '/indexPage',
-    text: '出访记录'
-  },
-  {
-    cid: 54,
-    to: '/indexPage',
-    text: '跟进记录'
-  },
-  {
-    cid: 55,
-    to: '/indexPage',
-    text: '导入客户'
-  }
-  ]
-},
-{
-  id: 4,
-  fontIcon: 'fa fa-calendar-check-o',
-  text: '客户审核'
+    id: 4,
+    fontIcon: 'fa fa-calendar-check-o',
+    text: '客户审核'
   //           cList: [
   //   { cid: 60, to: "/indexPage/rule", text: "权限设置" },
   //   { cid: 61, to: "/indexPage/auth", text: "角色设置" },
@@ -96,11 +97,11 @@ export const navList = [{
   //   { cid: 64, to: "/indexPage/accountManage", text: "账号管理" },
   //   { cid: 65, to: "/indexPage/news", text: "公告管理" },
   // ],
-},
-{
-  id: 5,
-  fontIcon: 'fa fa-search-plus',
-  text: '客户搜索'
+  },
+  {
+    id: 5,
+    fontIcon: 'fa fa-search-plus',
+    text: '客户搜索'
   //           cList: [
   //   { cid: 70, to: "/indexPage/rule", text: "权限设置" },
   //   { cid: 71, to: "/indexPage/auth", text: "角色设置" },
@@ -109,11 +110,11 @@ export const navList = [{
   //   { cid: 74, to: "/indexPage/accountManage", text: "账号管理" },
   //   { cid: 75, to: "/indexPage/news", text: "公告管理" },
   // ],
-},
-{
-  id: 6,
-  fontIcon: 'fa fa-file',
-  text: '合同管理'
+  },
+  {
+    id: 6,
+    fontIcon: 'fa fa-file',
+    text: '合同管理'
   //           cList: [
   //   { cid: 80, to: "/indexPage/rule", text: "权限设置" },
   //   { cid: 81, to: "/indexPage/auth", text: "角色设置" },
@@ -122,55 +123,55 @@ export const navList = [{
   //   { cid: 84, to: "/indexPage/accountManage", text: "账号管理" },
   //   { cid: 85, to: "/indexPage/news", text: "公告管理" },
   // ],
-},
-{
-  id: 7,
-  fontIcon: 'fa fa-cny',
-  text: '到款管理',
-  cList: [{
-    cid: 90,
-    to: '/indexPage/moneyRecord',
-    text: '到款记录'
   },
   {
-    cid: 91,
-    to: '/indexPage/bankflowList',
-    text: '银行流水列表'
+    id: 7,
+    fontIcon: 'fa fa-cny',
+    text: '到款管理',
+    cList: [{
+      cid: 90,
+      to: '/indexPage/moneyRecord',
+      text: '到款记录'
+    },
+    {
+      cid: 91,
+      to: '/indexPage/bankflowList',
+      text: '银行流水列表'
+    },
+    {
+      cid: 92,
+      to: '/indexPage/myBankflow',
+      text: '我的银行流水'
+    },
+    {
+      cid: 93,
+      to: '/indexPage/moneyCount',
+      text: '到款统计'
+    },
+    {
+      cid: 94,
+      to: '/indexPage/addFlow',
+      text: '录入流水'
+    }
+    ]
   },
   {
-    cid: 92,
-    to: '/indexPage/myBankflow',
-    text: '我的银行流水'
+    id: 8,
+    fontIcon: 'fa fa-clipboard',
+    text: '订单管理',
+    cList: [
+      // { cid: 100, to: '/indexPage/rule', text: '待处理订单' },
+      // { cid: 101, to: '/indexPage/auth', text: '已处理订单' },
+      // { cid: 102, to: '/indexPage/systemCycle', text: '订单列表' },
+      // { cid: 103, to: '/indexPage/departSetNum', text: '资金表' },
+      { cid: 104, to: '/indexPage/addBaiduOrder', text: '新增百度订单' }
+      // { cid: 105, to: '/indexPage/news', text: '新增网建订单' }
+    ]
   },
   {
-    cid: 93,
-    to: '/indexPage/moneyCount',
-    text: '到款统计'
-  },
-  {
-    cid: 94,
-    to: '/indexPage/addFlow',
-    text: '录入流水'
-  }
-  ]
-},
-{
-  id: 8,
-  fontIcon: 'fa fa-clipboard',
-  text: '订单管理'
-  //           cList: [
-  //   { cid: 100, to: "/indexPage/rule", text: "权限设置" },
-  //   { cid: 101, to: "/indexPage/auth", text: "角色设置" },
-  //   { cid: 102, to: "/indexPage/systemCycle", text: "时长设置" },
-  //   { cid: 103, to: "/indexPage/departSetNum", text: "部门配额" },
-  //   { cid: 104, to: "/indexPage/accountManage", text: "账号管理" },
-  //   { cid: 105, to: "/indexPage/news", text: "公告管理" },
-  // ],
-},
-{
-  id: 9,
-  fontIcon: 'fa fa-credit-card',
-  text: '续费管理'
+    id: 9,
+    fontIcon: 'fa fa-credit-card',
+    text: '续费管理'
   //           cList: [
   //   { cid: 110, to: "/indexPage/rule", text: "权限设置" },
   //   { cid: 111, to: "/indexPage/auth", text: "角色设置" },
@@ -179,11 +180,11 @@ export const navList = [{
   //   { cid: 114, to: "/indexPage/accountManage", text: "账号管理" },
   //   { cid: 115, to: "/indexPage/news", text: "公告管理" },
   // ],
-},
-{
-  id: 10,
-  fontIcon: 'fa fa-newspaper-o',
-  text: '发票管理'
+  },
+  {
+    id: 10,
+    fontIcon: 'fa fa-newspaper-o',
+    text: '发票管理'
   //           cList: [
   //   { cid: 120, to: "/indexPage/rule", text: "权限设置" },
   //   { cid: 121, to: "/indexPage/auth", text: "角色设置" },
@@ -192,11 +193,11 @@ export const navList = [{
   //   { cid: 124, to: "/indexPage/accountManage", text: "账号管理" },
   //   { cid: 125, to: "/indexPage/news", text: "公告管理" },
   // ],
-},
-{
-  id: 11,
-  fontIcon: 'fa fa-money',
-  text: '工资管理'
+  },
+  {
+    id: 11,
+    fontIcon: 'fa fa-money',
+    text: '工资管理'
   //           cList: [
   //   { cid: 130, to: "/indexPage/rule", text: "权限设置" },
   //   { cid: 131, to: "/indexPage/auth", text: "角色设置" },
@@ -205,5 +206,5 @@ export const navList = [{
   //   { cid: 134, to: "/indexPage/accountManage", text: "账号管理" },
   //   { cid: 135, to: "/indexPage/news", text: "公告管理" },
   // ],
-}
+  }
 ]
---
+++ src/components/order/addBaiduOrder/addBaiduOrder.vue
@@ -0,0 +1,212 @@
+<template>
+  <div class="addbaidu-order">
+    <p>
+      <span>订单管理 / 新增百度订单</span>
+    </p>
+    <div class="order-content">
+      <el-form ref="form" :model="form" label-width="180px" size="small">
+        <el-form-item label="公司名称" required>
+          <el-input v-model="form.cName" :disabled="true" style="width:300px;" placeholder="点击选择按钮选择公司"></el-input>
+          <el-button type="primary" @click.native="selCompany">选择</el-button>
+        </el-form-item>
+
+        <el-form-item label="到款记录" required>
+          <el-select v-model="form.record" placeholder="请选择到款记录" style="width:300px;">
+            <el-option label="区域一" value="shanghai"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="客户网站信息">
+          <el-input v-model="form.pcWeb" style="width:300px;" placeholder="pc端网址"></el-input>
+          <el-input v-model="form.phoneWeb" style="width:300px;" placeholder="手机端网址"></el-input>
+        </el-form-item>
+
+        <el-form-item label="联系人信息" required>
+          <el-row v-for="(item,index) in form.contactList" :key="index" :class="{'mt10px':index!==0}">
+            <el-input v-model="item.contact" placeholder="联系人" style="width:145px;">
+              <el-select v-model="item.contact" slot="append">
+                <el-option label="餐厅名" value="餐厅名"></el-option>
+                <el-option label="订单号" value="订单号"></el-option>
+                <el-option label="用户电话" value="用户电话"></el-option>
+              </el-select>
+            </el-input>
+            <el-input v-model="item.phoneNumber" style="width:150px;" placeholder="手机号码"></el-input>
+            <el-input v-model="item.fixedNumber" style="width:150px;" placeholder="座机号码"></el-input>
+            <el-input v-model="item.email" style="width:145px;" placeholder="邮箱"></el-input>
+            <el-button :type="index===0?'success':'danger'" size="mini" circle class="ml10px" :icon="index===0?isPlusIcon:isMinusIcon" @click.native="addContact(index)">
+            </el-button>
+          </el-row>
+        </el-form-item>
+
+        <el-form-item label="客户地址" required>
+          <el-input v-model="form.address" style="width:300px;" placeholder="客户地址"></el-input>
+        </el-form-item>
+
+        <el-form-item label="网建类型" required>
+          <el-radio-group v-model="form.wjType">
+            <el-radio label="客建双站"></el-radio>
+            <el-radio label="自建双站"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item label="客户类型" required>
+          <el-radio-group v-model="form.cusType">
+            <el-radio label="新客户"></el-radio>
+            <el-radio label="老客户"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item label="是否需要发票" required>
+          <el-radio-group v-model="form.isInvoice">
+            <el-radio label="不需要"></el-radio>
+            <el-radio label="暂不需要"></el-radio>
+            <el-radio label="需要"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item label="合同编号" required>
+          <el-select v-model="form.record" placeholder="请选择到款记录" style="width:198px;">
+            <el-option label="区域一" value="shanghai"></el-option>
+          </el-select>
+          <el-select v-model="form.record" placeholder="请选择到款记录" style="width:198px;">
+            <el-option label="区域一" value="shanghai"></el-option>
+          </el-select>
+          <el-select v-model="form.record" placeholder="请选择到款记录" style="width:198px;">
+            <el-option label="区域一" value="shanghai"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="商务信息" required>
+          <el-input v-model="form.shangWuName" :disabled="true" style="width:300px;"></el-input>
+        </el-form-item>
+        <el-form-item label="订单详情" required>
+          <el-tabs type="border-card" style="max-width:750px;">
+            <el-tab-pane label="企业资质">
+              <el-card shadow="always" class="card-tips">
+                [说明]：根据订单业务类型，上传需要的资质，图片格式为：jpg、png、jpeg，图片大小在3M以下。
+              </el-card>
+              <el-row style="margin-top:10px;">
+                <el-select v-model="form.zizhiName" style="width:80%;">
+                  <el-option label="餐厅名" value="1"></el-option>
+                  <el-option label="订单号" value="2"></el-option>
+                  <el-option label="用户电话" value="3"></el-option>
+                </el-select>
+                <el-button type="primary">添加资质</el-button>
+              </el-row>
+              <el-row style="margin-top:10px;">
+                <el-table border :data="form.zizhiList" style="width: 100%;min-height:200px;">
+                  <el-table-column prop="zizhiType" label="资质类型">
+                  </el-table-column>
+                  <el-table-column label="操作">
+                    <template slot-scope="scope">
+                      <!-- <el-button type="primary">上传</el-button> -->
+                      <el-upload class="upload-demo" ref="upload" action="https://jsonplaceholder.typicode.com/posts/" :on-preview="handlePreview" :on-remove="handleRemove" :file-list="fileList" :auto-upload="false">
+                        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+                        <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+                      </el-upload>
+                    </template>
+                  </el-table-column>
+                </el-table>
+              </el-row>
+            </el-tab-pane>
+            <el-tab-pane label="到款记录">
+              123
+            </el-tab-pane>
+          </el-tabs>
+        </el-form-item>
+
+        <el-form-item label="">
+          <el-row style="width:750px;text-align:right;">
+            <el-button type="warning">仅降E</el-button>
+            <el-button type="primary">降E并提单</el-button>
+          </el-row>
+        </el-form-item>
+      </el-form>
+      <!-- 选择公司弹窗 -->
+      <el-dialog title="选择下单客户" :visible.sync="selCompanyDialog">
+        <el-input placeholder="请输入公司名进行搜索" v-model="handleCompanyName">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-table :data="companyList" class="mt10px">
+          <el-table-column property="date" label="日期" width="150"></el-table-column>
+          <el-table-column property="name" label="姓名" width="200"></el-table-column>
+          <el-table-column property="address" label="地址"></el-table-column>
+        </el-table>
+      </el-dialog>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        cName: '',
+        record: '',
+        pcWeb: '',
+        phoneWeb: '',
+        contactList: [
+          {
+            contactId: '',
+            contact: '',
+            phoneNumber: '',
+            fixedNumber: ''
+          }
+        ],
+        email: '',
+        address: '',
+        wjType: '',
+        cusType: '',
+        isInvoice: '',
+        shangWuName: '',
+        zizhiName: '',
+        zizhiList: [
+          {
+            zizhiType: '企业直招'
+          }
+        ]
+      },
+      fileList: [],
+      isPlusIcon: 'el-icon-plus',
+      isMinusIcon: 'el-icon-minus',
+      selCompanyDialog: false,
+      handleCompanyName: ''
+    }
+  },
+  methods: {
+    selCompany () {
+      this.selCompanyDialog = true
+    },
+    addContact (index) {
+      if (index === 0) {
+        this.form.contactList.push({})
+      } else {
+        this.form.contactList.splice(index, 1)
+      }
+    },
+    submitUpload () {
+      this.$refs.upload.submit()
+    },
+    handleRemove (file, fileList) {
+      console.log(file, fileList)
+    },
+    handlePreview (file) {
+      console.log(file)
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang="less">
+.order-content {
+  background: #fff;
+  padding: 20px;
+  .card-tips {
+    color: red;
+    background: wheat;
+  }
+}
+</style>
