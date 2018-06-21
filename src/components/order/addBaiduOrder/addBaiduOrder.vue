e0fdee348ace312f4875b716f6554f9298015eb6
Jquank <478197650@qq.com>
2018-06-21 21:18:28 +0800

    11

--- src/components/order/addBaiduOrder/addBaiduOrder.vue
+++ src/components/order/addBaiduOrder/addBaiduOrder.vue
@@ -130,7 +130,7 @@
         <el-table :data="companyList" class="mt10px">
           <el-table-column property="date" label="日期" width="150"></el-table-column>
           <el-table-column property="name" label="姓名" width="200"></el-table-column>
-          <el-table-column property="address" label="地址"></el-table-column>
+          <el-table-column property="name" label="地址"></el-table-column>
         </el-table>
       </el-dialog>
 
@@ -172,7 +172,8 @@ export default {
       isPlusIcon: 'el-icon-plus',
       isMinusIcon: 'el-icon-minus',
       selCompanyDialog: false,
-      handleCompanyName: ''
+      handleCompanyName: '',
+      companyList: []
     }
   },
   methods: {
